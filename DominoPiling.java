0a5717293b31662872f87274003c1db76e226b96
kazimayaan <kazimayaan486@gmail.com>
2023-09-22 00:46:26 +0530

    cf probs

---
+++ DominoPiling.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class DominoPiling {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int m = in.nextInt();
+        int n = in.nextInt();
+        // int d = 2;
+       int area = m*n;
+       int sol = area/2;
+       System.out.println(sol);
+    }
+}
--- NextRound.java
+++ NextRound.java
@@ -1,3 +1,27 @@
+import java.util.Scanner;
+
 public class NextRound {
-    
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int k = in.nextInt();
+        int counter = 0;
+        int j = 1;
+        int criminal = 0;
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+            if (i == k - 1) {
+                criminal = arr[i];
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (arr[i] >= criminal && arr[i] > 0) {
+                counter++;
+            } else {
+                break;
+            }
+        }
+        System.out.println(counter);
+    }
 }
