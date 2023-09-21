900b7cb43e49b9d16b94f4306552221341dc2fcf
kazimayaan <kazimayaan486@gmail.com>
2023-09-22 00:54:20 +0530

    new prob

--- BitPlusPlus.java
+++ BitPlusPlus.java
@@ -1,3 +1,19 @@
+import java.util.Scanner;
+
 public class BitPlusPlus {
-    
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int counter = 0;
+        for(int i = 0; i< n; i++){
+            String str = in.next();
+            if(str.contains("+")){
+                counter++;
+            }
+            else{
+                counter--;
+            }
+        }
+        System.out.println(counter);
+    }
 }
